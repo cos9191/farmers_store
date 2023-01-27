98a4443f91b77601faea3d9256535746105752de
Konstantin Fedorov <105093851+cos9191@users.noreply.github.com>
2023-01-27 19:42:00 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 import { StrictMode } from "react";
 import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "/src/themes/default";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
 import App from "/src/components/app/App";
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <StrictMode>
     <ThemeProvider theme={defaultTheme}>
       <App />
     </ThemeProvider>
-  </StrictMode>,
-  rootElement
+  </StrictMode>
 );
